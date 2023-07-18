0c3da11f342eba7074bbfd6d2ce8cf43a82e41e9
harish0033 <139841439+harish0033@users.noreply.github.com>
2023-07-18 22:11:56 +0530

    Add files via upload

---
+++ Employeedetails.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class Employeedetails {
+	public static void main(String[] args) {
+		String name = "yogin";
+		int EmployeeID = 51243;
+		byte age = 23;
+		float Experience = 3.5f;
+		String Gender ="Male";
+		int salary = 40000;
+		System.out.println("Name -- "+name);
+		System.out.println("Age -- "+age);
+		System.out.println("Employee ID -- " +EmployeeID);
+		System.out.println("Experience -- "+Experience + " Year");
+		System.out.println("Gender -- "+Gender);
+		System.out.println("Salary -- "+salary);
+
+	}
+
+}
